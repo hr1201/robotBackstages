582b87c85ac0bf1eaea5733babb2efe687a08982
hr121 <568127468@qq.com>
2023-10-24 11:14:27 +0800

    修改接口地址

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,77 +1,79 @@
 import http from "../utils/request";
 
+let urls: string = 'https://api.softwolf.cn'
+
 // 登录
 export const login = (username: string, password: string) => {
-    return http.post('groups/loginManagement', { groupName: username, password: password })
+    return http.post(urls + urls + '/groups/loginManagement', { groupName: username, password: password })
 }
 
 // 组员管理页面获取组员
 export const getMember = (groupId: number) => {
-    return http.get('/logins/getAllTeamMember', { params: { groupId: groupId } })
+    return http.get(urls + '/logins/getAllTeamMember', { params: { groupId: groupId } })
 }
 
 // 组员管理页面删除
 export const deleteMember = (id: number) => {
-    let url = `/users/${id}`
+    let url = urls + `/users/${id}`
     return http.delete(url)
 }
 
 // 组员管理页面修改
 export const editMember = (menber: object) => {
-    return http.put('/logins', { ...menber })
+    return http.put(urls + '/logins', { ...menber })
 }
 
 // 获取用户所有动态
 export const getDynamic = () => {
-    return http.get('/Share/getAll')
+    return http.get(urls + '/Share/getAll')
 }
 
 // 获取野狼组员的任务
 export const getPlan = (date: string, groupid: string) => {
-    return http.get('/tasks/getTasksByGroupIdAndDate', { params: { date: date, groupId: groupid } })
+    return http.get(urls + '/tasks/getTasksByGroupIdAndDate', { params: { date: date, groupId: groupid } })
 }
 
 // 获取指定野狼组员的任务
 export const getownPlan = (name: string) => {
-    return http.get('/tasks/getSearchUserTask', { params: { userName: name } })
+    return http.get(urls + '/tasks/getSearchUserTask', { params: { userName: name } })
 }
 
 // 获取用户时长
 export const getDuration = (groupId: number) => {
-    return http.get('/logins', { params: { groupId: groupId } })
+    return http.get(urls + '/logins', { params: { groupId: groupId } })
 }
 
 // 获取该组的周报
 export const getWeekly = (week: string, groupId: number) => {
-    return http.get('/WeekSummary/getUrlByWeekGroupId', { params: { week: week, groupId: groupId } })
+    return http.get(urls + '/WeekSummary/getUrlByWeekGroupId', { params: { week: week, groupId: groupId } })
 }
 
 // 获取所有的周期
 export const getWeek = () => {
-    return http.get('/WeekSummary/getWeek')
+    return http.get(urls + '/WeekSummary/getWeek')
 }
 
 // 获取组内用户以及用户上周时长
 export const getGroupAllUserDuration = (groupId: number) => {
-    return http.get('/logins/getGroupAllUserDuration', { params: { groupId: groupId } })
+    return http.get(urls + '/logins/getGroupAllUserDuration', { params: { groupId: groupId } })
 }
 
 // 获取用户的头像
 export const getGroupInfo = (groupId: number) => {
-    return http.get('/groups/getGroupInfo', { params: { groupId: groupId } })
+    return http.get(urls + '/groups/getGroupInfo', { params: { groupId: groupId } })
 }
 
 // 设置正式队员的任务
-export const setTask = (task:object) => {
-    return http.post('/LeaderTask/setFormalTeamMemberLeaderTask', { ...task })
+export const setTask = (task: object) => {
+    return http.post(urls + '/LeaderTask/setFormalTeamMemberLeaderTask', { ...task })
 }
 
 // 保存或修改组别任务
-export const saveGroupTask = (task:object) => {
-    return http.post('/groups/saveGroupTask', { ...task })
+export const saveGroupTask = (task: object) => {
+    return http.post(urls + '/groups/saveGroupTask', { ...task })
 }
 
 // 获取组别本周任务
 export const getGroupWeekTask = (groupId: number) => {
-    return http.get('/groups/getGroupWeekTask', { params: { groupId: groupId } })
+    return http.get(urls + '/groups/getGroupWeekTask', { params: { groupId: groupId } })
 }
