18f2f5194e0303e8180aef976fc744d8e9f6e0b3
hr121 <568127468@qq.com>
2023-10-24 11:28:12 +0800

    修改3

--- src/http/index.ts
+++ src/http/index.ts
@@ -4,7 +4,7 @@ let urls: string = 'https://api.softwolf.cn'
 
 // 登录
 export const login = (username: string, password: string) => {
-    return http.post(urls + urls + '/groups/loginManagement', { groupName: username, password: password })
+    return http.post(urls + '/groups/loginManagement', { groupName: username, password: password })
 }
 
 // 组员管理页面获取组员
