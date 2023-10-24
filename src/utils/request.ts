cc38aca3e6c54e91b1a5a665437b7ca0c9eb7bbc
hr121 <568127468@qq.com>
2023-10-24 11:20:14 +0800

    推送dist

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'api';
 } else {
   //生产环境 do something
-  axios.defaults.baseURL = '';
+  axios.defaults.baseURL = 'api';
 }
 
 const http = axios.create({
