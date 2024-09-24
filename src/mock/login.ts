67ac8275d52a3785b9a47492e3d69dd9327528ee
hr121 <568127468@qq.com>
2024-09-24 17:32:06 +0800

    登录接口切换为easy-mock接口

--- src/mock/login.ts
+++ src/mock/login.ts
@@ -1,46 +1,46 @@
-import Mock from 'mockjs'
+// import Mock from 'mockjs'
 
-export default [
-  {
-    url: '/api/groups/loginManagement',
-    method: 'post',
-    response: ({ body }: { body: any }) => {
-      const { groupName, password } = body
-      if (groupName === 'Rarrot' && password === 'Rarrot') {
-        return {
-          code: 200,
-          data: {
-            token: Mock.mock('@guid'), // 使用 mockjs 生成一个模拟的 token
-            groupId: Mock.mock('@integer(1, 8)'), // 使用 mockjs 生成一个模拟的 groupId
-          }
-        }
-      } else {
-        return {
-          code: 400,
-          message: 'Invalid username or password'
-        }
-      }
-    }
-  },
-  {
-    url: '/api/groups/getGroupInfo',
-    method: 'get',
-    response: ({ query }: { query: any }) => {
-      const { groupId } = query
-      if (groupId) {
-        return {
-          code: 200,
-          data: {
-            headUrl: Mock.mock('@image'), // 使用 mockjs 生成一个模拟的图片 URL
-            // ...其他组信息
-          }
-        }
-      } else {
-        return {
-          code: 400,
-          message: 'Invalid groupId'
-        }
-      }
-    }
-  }
-]
+// export default [
+//   {
+//     url: '/api/groups/loginManagement',
+//     method: 'post',
+//     response: ({ body }: { body: any }) => {
+//       const { groupName, password } = body
+//       if (groupName === 'Rarrot' && password === 'Rarrot') {
+//         return {
+//           code: 200,
+//           data: {
+//             token: Mock.mock('@guid'), // 使用 mockjs 生成一个模拟的 token
+//             groupId: Mock.mock('@integer(1, 8)'), // 使用 mockjs 生成一个模拟的 groupId
+//           }
+//         }
+//       } else {
+//         return {
+//           code: 400,
+//           message: 'Invalid username or password'
+//         }
+//       }
+//     }
+//   },
+//   {
+//     url: '/api/groups/getGroupInfo',
+//     method: 'get',
+//     response: ({ query }: { query: any }) => {
+//       const { groupId } = query
+//       if (groupId) {
+//         return {
+//           code: 200,
+//           data: {
+//             headUrl: Mock.mock('@image'), // 使用 mockjs 生成一个模拟的图片 URL
+//             // ...其他组信息
+//           }
+//         }
+//       } else {
+//         return {
+//           code: 400,
+//           message: 'Invalid groupId'
+//         }
+//       }
+//     }
+//   }
+// ]
